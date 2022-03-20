64dac43486d8eab438be5bab7a3deae3d901503a
Vrushit7506 <vrushit7506@gmail.com>
2022-03-21 00:23:41 +0530

    Final Changes

--- sqpub/settings.py
+++ sqpub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm-+0!!@pamax2+hh$kn4$p5aysq^sn@m5s5*a*znw5j@c4e^@+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
